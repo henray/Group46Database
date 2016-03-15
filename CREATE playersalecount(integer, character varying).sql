c81bd77da755584c59dc0e52b708d6ab3e20632b
dolphinpink <iotacubed@gmail.com>
2016-03-14 22:06:14 -0400

    Added files via upload

---
+++ CREATE playersalecount(integer, character varying).sql
@@ -0,0 +1,26 @@
+﻿-- Function: public.playersalecount(integer, character varying)
+
+-- DROP FUNCTION public.playersalecount(integer, character varying);
+
+CREATE OR REPLACE FUNCTION public.playersalecount(
+    playernum integer,
+    playerteam character varying)
+  RETURNS integer AS
+$BODY$
+DECLARE
+	customerCount int;
+BEGIN	
+	customerCount := 	(SELECT sum(quantity) -- orders with player products
+				FROM ProductOrderWarehouse
+				JOIN ( 	SELECT productId AS playerId -- product ids associated with player
+					FROM PlayerMerchandisePlayer
+					WHERE playernumber = playerNum AND teamname = playerTeam) AS playerProducts
+				ON ProductOrderWarehouse.productId = playerProducts.playerId);
+
+	RETURN customerCount;
+
+END$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.playersalecount(integer, character varying)
+  OWNER TO postgres;
